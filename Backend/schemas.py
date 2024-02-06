db47234c4f22970db5e8c41165d8b2e8eaaea3d3
JorgeUrias123 <152941899+JorgeUrias123@users.noreply.github.com>
2024-02-06 03:50:16 -0700

    Update schemas.py

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -1,5 +1,36 @@
 from pydantic import BaseModel
+from .models import GeneroEnum, EstadoEnum
+from datetime import date
 
 
-class CrearRol(BaseModel):
+class crear_rol(BaseModel):
   nombre: str
+
+
+class crear_usuario(BaseModel):
+  nombre: str
+  apellido: str
+  apellido2: str
+  correo: str
+  contraseña: str
+  recontraseña: str
+  telefono: str
+  genero: GeneroEnum
+  edad: int
+  rol: str
+
+
+class crear_torneo(BaseModel):
+  nombre: str
+  fecha_inicio: date
+  fecha_fin: date
+  estado: EstadoEnum
+
+
+class crear_escuela(BaseModel):
+  nombre_escuela: str
+  nombre_maestro: str
+  telefono: str
+  correo: str
+  contraseña: str
+  recontraseña: str
