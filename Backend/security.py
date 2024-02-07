af3a83c4c3e759a7735f04a06d11c53750ea1013
JorgeUrias123 <152941899+JorgeUrias123@users.noreply.github.com>
2024-02-06 22:28:39 -0700

    Update security.py

--- Backend/security.py
+++ Backend/security.py
@@ -1,4 +1,23 @@
 import bcrypt
+import jwt
+from datetime import datetime, timedelta
+from typing import Optional
+
+SECRET_KEY = "PC_JOYAS"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# FUNCION PARA CREAR EL TOKEN DE ACCESO
+def crear_token_acceso(data: dict, expires_delta: Optional[timedelta] = None):
+  to_encode = data.copy()
+  if expires_delta:
+    expire = datetime.utcnow() + expires_delta
+  else:
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+  
+  to_encode.update({"exp": expire})
+  encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+  return encode_jwt
 
 # ENCRIPTAR CONTRASEÑA
 def hash_password(password: str) -> str:
